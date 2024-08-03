b2f154d34410b6369b70b658d0848b0bce3cd438
caligiure <caligiure74@gmail.com>
2024-08-03 18:57:49 +0200

    The board gridLayout is working

--- src/Test.java
+++ src/Test.java
@@ -2,13 +2,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Test extends JFrame {
-    public static final int ROWS = 3, COLUMNS=8;
 
     private void printInfo(String s){
         System.out.println(s);
     }
 
     public Test() {
+        final int ROWS = 10, COLUMNS = 10;
         // initialize the window
         setTitle("Scale e Serpenti");
         setSize(600, 600);
@@ -18,22 +18,33 @@ public class Test extends JFrame {
         // create the board with GridLayout
         JPanel boardPanel = new JPanel(new GridLayout(ROWS, COLUMNS));
         JLabel[][] cells = new JLabel[ROWS][COLUMNS]; // create matrix of labels
-        // fill the board (from last to first cell)
-        for(int i = 0; i < ROWS; i++) {
-            for(int j = 0; j < COLUMNS; j++) {
-                int cellLabel = (ROWS - i - 1) * COLUMNS + j +1;
-                if (i % 2 == 1)
-                    cellLabel = (ROWS - i) * COLUMNS - j;
-                cells[i][j] = new JLabel(String.valueOf(cellLabel), SwingConstants.CENTER);
-                cells[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
-                cells[i][j].setOpaque(true); //CONTROLLA POI
-                printInfo("Adding cell " + cellLabel + " to board");
-                boardPanel.add(cells[i][j]);
-            }
-        }
+        fillTheBoard(boardPanel, cells, ROWS, COLUMNS);
         // add the board component to the container
         add(boardPanel, BorderLayout.CENTER);
 
+        // add a button to roll the dice (Only in manual mode)
+        JButton rollButton = new JButton("Roll Dice");
+        rollButton.addActionListener(_ -> rollDice());
+        add(rollButton, BorderLayout.SOUTH);
+
+    }
+
+    private static void fillTheBoard(JPanel boardPanel, JLabel[][] cells, int row, int col) {
+        // fill the board from last to first cell
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                int cellLabel = (row - i - 1) * col + j +1;
+                if (row % 2 == 1 &&  i % 2 == 1 || row % 2 == 0 &&  i % 2 == 0)
+                    cellLabel = (row - i) * col - j;
+                cells[i][j] = new JLabel(String.valueOf(cellLabel), SwingConstants.CENTER);
+                cells[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                cells[i][j].setOpaque(true); //CONTROLLA POI
+                boardPanel.add(cells[i][j]);
+            }
+        }
+    }
+
+    private static void rollDice(){
 
     }
 
