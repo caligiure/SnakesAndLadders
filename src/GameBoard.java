c2aa9b2b382c7fda2d5788467a6b5d9689d31633
caligiure <caligiure74@gmail.com>
2024-08-28 14:14:31 +0200

    Working on gamelog

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 public class GameBoard {
@@ -9,14 +10,19 @@ public class GameBoard {
     private BoardFrame boardFrame;
     private JLabel[][] cells;
     // Players
-    private String[] playersName;
+    private final String[] playersID;
+    private final String[] playersName;
     private int[] playersPosition;
     private int nextPlayer; // indicates the number of the player who must roll the dices
+    // Game Log
+    private DefaultTableModel playersTable;  // shows ID, name and position of every player
+    private JTextArea gameLog;  // Area di testo per il resoconto del gioco
 
     public GameBoard(PrimaryRulesRecord primaryRules, SpecialRulesRecord specialRules) {
         this.primaryRules = primaryRules;
         this.specialRules = specialRules;
         playersName = new String[primaryRules.nPlayers()]; // da cambiare con il passaggio dei nomi da GameConfiguration
+        playersID = new String[primaryRules.nPlayers()];
         playersPosition = new int[primaryRules.nPlayers()];
         nextPlayer = 0;
         boardFrame = new BoardFrame();
@@ -40,15 +46,19 @@ public class GameBoard {
             add(boardPanel, BorderLayout.CENTER);
 
             // creates a panel which will hold all the buttons
-            JPanel buttonsPanel = getButtonsPanel();
+            JPanel buttonsPanel = buildButtonsPanel();
             // add the buttons panel to the frame
             add(buttonsPanel, BorderLayout.SOUTH);
 
             // add the players to the board
             initializePlayers();
+
+            // create a side panel to show game log and players table
+            JPanel sidePanel = buildSidePanel();
+            add(sidePanel, BorderLayout.EAST);
         }
 
-        private JPanel getButtonsPanel() {
+        private JPanel buildButtonsPanel() {
             JPanel buttonsPanel = new JPanel(new GridLayout(1, 2, 10, 10));
             // add exit button
             JButton exitButton = new JButton("Exit");
@@ -61,6 +71,53 @@ public class GameBoard {
                 buttonsPanel.add(rollButton);
             }
             return buttonsPanel;
+        }
+
+        private JPanel buildSidePanel() {
+            JPanel sidePanel = new JPanel();
+            sidePanel.setLayout(new BorderLayout());
+
+            JScrollPane pTable = buildPlayersTable(); // create players table
+            sidePanel.add(pTable, BorderLayout.NORTH); // add the table to the side panel
+
+            JScrollPane logScrollPane = buildGameLog();
+            sidePanel.add(logScrollPane, BorderLayout.CENTER);
+
+            return sidePanel;
+        }
+
+        private JScrollPane buildGameLog() {
+            gameLog = new JTextArea(10, 20);
+            gameLog.setEditable(false);
+            gameLog.append("Current Player: " + playersName[0] + ".\n");
+            return new JScrollPane(gameLog);
+        }
+
+        private JScrollPane buildPlayersTable() {
+            // Side table with players ID and name
+            String[] columnNames = {"ID", "Name", "Pos."};
+            Object[][] data = new Object[primaryRules.nPlayers()][3];
+            for (int i = 0; i < primaryRules.nPlayers(); i++) {
+                data[i][0] = playersID[i];
+                data[i][1] = playersName[i];
+                data[i][2] = playersPosition[i];
+            }
+            playersTable = new DefaultTableModel(data, columnNames);
+            JTable table = new JTable(playersTable); // contains the default table model
+            return new JScrollPane(table); // JScrollPane makes the table scrollable
+        }
+
+        private void updatePlayersTable(int playerIndex, int newPosition) {
+            playersTable.setValueAt(playerIndex, 2, newPosition);
+        }
+
+        private void initializePlayers() {
+            for(int i=0; i<primaryRules.nPlayers(); i++){
+                String id = "👤"+(i+1);
+                playersID[i] = id;
+                playersPosition[i] = 1; // the starting point for every player is cell 1
+                updatePlayerPosition(i, 1);
+            }
         }
 
         private void exitGame() {
@@ -81,7 +138,8 @@ public class GameBoard {
             for(int i = 0; i < rows; i++) {
                 for(int j = 0; j < cols; j++) {
                     int cellLabel = indexPosition(i, j);
-                    cells[i][j] = new JLabel(String.valueOf(cellLabel), SwingConstants.CENTER);
+                    String text = "<html>" + cellLabel + ")</html>";
+                    cells[i][j] = new JLabel(text, SwingConstants.CENTER);
                     cells[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
                     //cells[i][j].setOpaque(true); //CONTROLLA POI
                     boardPanel.add(cells[i][j]);
@@ -124,42 +182,38 @@ public class GameBoard {
             return cells[cord[0]][cord[1]];
         }
 
-        private void initializePlayers() {
-            for(int i=0; i<primaryRules.nPlayers(); i++){
-                String name = "👤"+(i+1); // questo non sarà necessario dopo aver importato i giocatori da GameConfig
-                playersName[i] = name;
-                playersPosition[i] = 1; // the starting point for every player is cell 1
-                updatePlayerPosition(i, 1);
-            }
-        }
-
         private void updatePlayerPosition(int playerIndex, int newPosition) {
             // remove player from the old cell
             int oldPosition = playersPosition[playerIndex];
             if(oldPosition >= 1) {
                 JLabel cell = getCellLabel(oldPosition);
-                StringBuilder newText = new StringBuilder(oldPosition + ".");
+                StringBuilder newText = new StringBuilder("<html>"+oldPosition + ")");
                 for (int otherPlayer = 0; otherPlayer < primaryRules.nPlayers(); otherPlayer++) {
                     if (otherPlayer != playerIndex && playersPosition[otherPlayer] == oldPosition)
-                        newText.append(" ").append(playersName[otherPlayer]); // the other players must be re-added if they are still in the old position
+                        newText.append(" ").append(playersID[otherPlayer]); // the other players must be re-added if they are still in the old position
                 }
+                newText.append("</html>");
                 cell.setText(newText.toString());
             }
             // add the player to the new position
             playersPosition[playerIndex] = newPosition;
             JLabel cell = getCellLabel(newPosition);
-            StringBuilder newText = new StringBuilder(newPosition + ".");
+            StringBuilder newText = new StringBuilder("<html>"+newPosition + ")");
             for(int player = 0; player < primaryRules.nPlayers(); player++) {
                 if(playersPosition[player] == newPosition)
-                    newText.append(" ").append(playersName[player]); // the other players must be re-added if they were in this cell
+                    newText.append(" ").append(playersID[player]); // the other players must be re-added if they were in this cell
             }
+            newText.append("</html>");
             cell.setText(newText.toString());
+            // update the players table
+            updatePlayersTable(playerIndex, newPosition);
         }
 
         private void nextTurn() { // updates the turn and makes a player roll the dice
             int currentPlayer = nextPlayer;
             nextPlayer = (nextPlayer +1) % primaryRules.nPlayers();
             rollDice(currentPlayer);
+            gameLog.append("Current Player: " + playersName[nextPlayer] + ".\n");
         }
 
         private void rollDice(int playerIndex){
