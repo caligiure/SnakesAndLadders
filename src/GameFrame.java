43f20e84cd89a401c156668a9a7e874b5624f306
caligiure <caligiure74@gmail.com>
2024-09-01 17:08:45 +0200

    Added rules frame

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -4,8 +4,6 @@ import java.awt.*;
 import java.util.LinkedList;
 import java.util.Random;
 
-// Da fare in futuro: autoAdvance, rules, reset button, executables
-
 class GameFrame extends JFrame { // GameFrame contains all the graphic elements of the game
     private final PrimaryRulesRecord primaryRules;
     private final SpecialRulesRecord specialRules;
@@ -255,8 +253,42 @@ class GameFrame extends JFrame { // GameFrame contains all the graphic elements 
     } // builds the bottom panel containing the buttons
 
     private void showRules() {
-        // da fare in futuro
+        JFrame rulesFrame = new JFrame("Snakes and Ladders Rules"); // Create a new JFrame for the rules
+        rulesFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        rulesFrame.setSize(1000, 500);
+        rulesFrame.setLocationRelativeTo(null); // set the frame in the center of the screen
+        JTextArea rulesTextArea = new JTextArea(); // create a JTextArea to display the rules
+        rulesTextArea.setFont(gameLog.getFont().deriveFont(17f));
+        rulesTextArea.setText(getGameRules());
+        rulesTextArea.setEditable(false); // text area is read-only
+        JScrollPane scrollPane = new JScrollPane(rulesTextArea); // JScrollPane in case the text is too long
+        rulesFrame.add(scrollPane);
+        rulesFrame.setVisible(true);
     } // shows the game rules
+
+    private String getGameRules() {
+        return """
+                Snakes and Ladders Rules:
+                1.  Each player starts at the bottom left corner (tile 1).
+                2.  Players take turns rolling the dice and moving forward by the sum of the numbers rolled.
+                3.  The first player to reach the last cell wins the game, but it must be reached with an exact number of steps.
+                     If a player rolls a number that would take them beyond the last cell, they get to the last cell and then
+                     retreat by the number of steps in excess.
+                4.  If a player lands on the bottom of a ladder (GREEN tiles), they move up to the top of the ladder ⏫.
+                5.  If a player lands on the head of a snake (RED tiles), they slide down to the tail of the snake ⏬.
+                6.  If a player lands on a BLUE tile, they must ROLL AGAIN the dice ↺⚀⚅.
+                7.  If a player lands on a CYAN tile, they must MOVE AGAIN ⏩ by the same number of steps they already moved.
+                8.  If a player lands on a MAGENTA tile, they must STOP ❌.
+                     A stopped player will have to wait ⏳ for a turn before being able to move again.
+                     A player can avoid getting stopped if they have a DENY STOP ✋ card.
+                9.  If a player lands on a YELLOW tile, they must DRAW A CARD ♠♣♥♦.
+                     There are 4 types of cards: ROLL AGAIN ↺⚀⚅, MOVE AGAIN ⏩, STOP ❌ and DENY STOP ✋.
+                     The first 3 types of card have the same effect of the tile with the same name.
+                     The DENY STOP card is a special card that the player can hold in their hands until needed.
+                     When that player gets stopped, they will consume their DENY STOP ✋ card and avoid getting stopped.
+                10. If a player rolled a DOUBLE SIX, at the end of their turn they will roll the dice a second time and move again.
+                """;
+    }
 
     private void exitGame() {
         int result = JOptionPane.showConfirmDialog(
